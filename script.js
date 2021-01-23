54958d6b67648069da626db98d44afe5308614da
Phlipper <marzullop73@gmail.com>
2021-01-23 14:50:53 -0500

    added hourUpdate function to check on current hour

--- script.js
+++ script.js
@@ -7,11 +7,16 @@ $(".saveBtn").on("click", function() {
     var time = $(this).parent().attr("id")
     console.log (time)
 
-var toDo = $(this).siblings(".description").val()
+    var toDo = $(this).siblings(".description").val()
     console.log (toDo)
     localStorage.setItem(time, toDo);
 
   });
 
+  function hourUpdate() {
+
+    var currentHour = moment().hours();
+    
+  }
 
 })
