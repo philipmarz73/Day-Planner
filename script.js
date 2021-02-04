bf76b576e373d94e78ae721357ff2f3bdb6409fa
Phlipper <marzullop73@gmail.com>
2021-02-04 14:33:26 -0500

    adding format to current day displayer

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // 
 
-$(document).ready(function(){
-$("#currentDay").text(moment())
+$(document).ready(function() {
+$("#currentDay").text(moment().format("dddd, MMMM Do"));
 
 $(".saveBtn").on("click", function() {
     var time = $(this).parent().attr("id")
@@ -20,12 +20,12 @@ $(".saveBtn").on("click", function() {
     $(".time-block").each(function() {
       var blockHour = parseInt($(this).attr("id").split("-")[1]);
 
-      if (blockHour === currentHour) {
-        $(this).addClass("present");
-      }
-      else if (blockHour < currentHour) {
-        $(this).removeClass("present");
+      if (blockHour < currentHour) {
         $(this).addClass("past");
+      }
+      else if (blockHour === currentHour) {
+        $(this).removeClass("past");
+        $(this).addClass("present");
       }
       else {
         $(this).removeClass("past");
@@ -35,19 +35,19 @@ $(".saveBtn").on("click", function() {
     });
   }
 
+    hourUpdate();
 
+    var interval = setInterval(hourUpdate, 15000);
 
-    var interval = setInterval(hourUpdate, 30,000);
+    $("#hour-9 .description").val(localStorage.getItem("hour-9"));
+    $("#hour-10 .description").val(localStorage.getItem("hour-10"));
+    $("#hour-11 .description").val(localStorage.getItem("hour-11"));
+    $("#hour-12 .description").val(localStorage.getItem("hour-12"));
+    $("#hour-13 .description").val(localStorage.getItem("hour-13"));
+    $("#hour-14 .description").val(localStorage.getItem("hour-14"));
+    $("#hour-15 .description").val(localStorage.getItem("hour-15"));
+    $("#hour-16 .description").val(localStorage.getItem("hour-16"));
+    $("#hour-17 .description").val(localStorage.getItem("hour-17"));
+  
+  });
 
-    $("#hour-9 .description").val(localStorage.getItem("hour-9"));
-    $("#hour-9 .description").val(localStorage.getItem("hour-9"));
-    $("#hour-9 .description").val(localStorage.getItem("hour-9"));
-    $("#hour-9 .description").val(localStorage.getItem("hour-9"));
-    $("#hour-9 .description").val(localStorage.getItem("hour-9"));
-    $("#hour-9 .description").val(localStorage.getItem("hour-9"));
-    $("#hour-9 .description").val(localStorage.getItem("hour-9"));
-    $("#hour-9 .description").val(localStorage.getItem("hour-9"));
-    $("#hour-9 .description").val(localStorage.getItem("hour-9"));
-  }
-
-})
